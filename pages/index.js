60b0a3a8b5aca4146ee2d5a0acbb50c1e3457094
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-20 19:30:04 -0600

    data

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({title}) {
+export default function Home({data}) {
   return (
     <div className={styles.container}>
       
@@ -26,6 +26,15 @@ export default function Home({title}) {
       </header>
 
       <main className={styles.main}>
+
+        {
+          data.map(
+            ev => <a href={`/events/${ev.id}`}>
+              <Image src={ev.image}/>
+            </a>
+          )
+        }
+
         <a href=''>
           <img/>
           <h2>Events in London</h2>
@@ -52,12 +61,15 @@ export default function Home({title}) {
 }
 
 
-export function getServerSideProps(){
-  
+export async function getServerSideProps(){
+
+  const { events_categories } = await import('/data/data.json')
+
+
+
   return{
     props:{
-      title: "Hello everyone",
-
+      data: events_categories,
     }
   }
 
