d8b657b5541c2002ead1ead6a936a175370f259b
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-20 18:40:45 -0600

    setting data

---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,15 @@
+
+const index = () => {
+  return (
+    <div>
+        <h2>Events in ...</h2>
+
+        <a href="/event/event1">Event 1</a>
+        <a href="/event/event2">Event 2</a>
+        <a href="/event/event3">Event 3</a>
+
+    </div>
+  )
+}
+
+export default index
