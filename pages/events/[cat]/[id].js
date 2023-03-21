0c567bacce4b2756b1d2fa42a46d67ab9a131176
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-20 22:39:12 -0600

    Update [id].js

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -9,3 +9,28 @@ const Page = () => {
 }
 
 export default Page;
+
+export async function getStaticPaths(){
+
+  const data = await import('/data/data.json');
+  const allEvents = data;
+
+  const allPaths = allEvents.map( path => {
+    return{
+      params:{
+        cat: path.city,
+        id: path.id
+      }
+    }
+  })
+
+  return{
+    paths: allPaths,
+    fallback: false
+  }
+
+}
+
+export async function getStaticProps(context){
+
+}
