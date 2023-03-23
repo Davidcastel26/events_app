d244f3d08e825a5bdecf4316c5535d0c0e693ec4
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-22 18:37:20 -0600

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <nav>
+          <img src="" alt="" />
+          <Link href="/"> Home</Link>
+          <Link href="/events"> Events</Link>
+          <Link href="/about"> About Us</Link>
+        </nav>
+      </header>
   )
 }
