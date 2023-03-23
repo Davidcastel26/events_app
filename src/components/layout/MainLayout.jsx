74f747add189183eb33a0e336cf4c91ac79cf3f8
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-22 18:50:37 -0600

    Create MainLayout.jsx

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Footer } from "../footer/Footer"
+import { Header } from "../header/Header"
+
+
+export const MainLayout = ({children}) => {
+  return (
+    <>
+        <Header />
+
+        <Footer />   
+    </>
+  )
+}
