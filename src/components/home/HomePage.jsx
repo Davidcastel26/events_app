1a5144db22fa2ad51392a32a9a0ed659f7764d37
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-22 18:34:56 -0600

    Update HomePage.jsx

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,6 +1,17 @@
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <main className={styles.main}>
+
+        {
+          data.map((ev) => (
+            <Link key={ev.id} href={`/events/${ev.id}`}>
+              <Image width={200} height={200} src={ev.image} alt={ev.title}/>
+              <h2>{ev.title}</h2>
+              <p>{ev.description}</p>
+            </Link>
+          ))
+        }
+      </main>
   )
 }
