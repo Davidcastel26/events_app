014f12a326ba58f8cf1bbe178972c26f65128b2e
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-02 15:52:43 -0600

    email form api

--- src/components/events/SingleEvents.jsx
+++ src/components/events/SingleEvents.jsx
@@ -1,16 +1,30 @@
 import Image from "next/image"
 
 export const SingleEvents = ({data}) => {
+  
+  const onSubmitt = () => {
+    console.log('holi');
+  }
+  
   return (
-    <div>
+    <div className="events_single_page">
         <Image src={ data.image} width={900} height={500} alt={data.title} />
         <h1>{data.title}</h1>
         <p>{data.description}</p>
-        <div>
+        <form onSubmit={onSubmitt} className="email_registration">
           <label> Enroll for this event!</label>
-          <input type="email" />
-          <button className="submit-id">submit</button>
-        </div>
+          <input  
+            id="submit-id-email"
+            placeholder="Please insert your email" 
+            type="email" 
+          />
+          <button 
+            type="button"
+            className="submit-id"
+          >
+            submit
+          </button>
+        </form>
     </div>
   )
 }
