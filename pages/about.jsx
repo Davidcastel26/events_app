597048f5d30fdfa24f984c108a5cd10d1c4d4915
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-02 14:53:50 -0600

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,13 +1,27 @@
 const Page = () => {
     return(
         <div className="about_page">
-            <h1 className="title_about_us">About us Page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?</p>
-            <h2 className="title_about_us">Lorem ipsum dolo</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?</p>
+            <h1 className="title_about_us">
+                About us Page
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?
+            </p>
+            <h2 className="title_about_us">
+                Lorem ipsum dolo
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?
+            </p>
             <ul className="about_us_list">
-                <li><h3>Recusandae quia eos dolor vero</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?</p></li>
+                <li>
+                    <h3>
+                        Recusandae quia eos dolor vero
+                    </h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?
+                </p>
+            </li>
             <li>
             <h3>At non eos laborum</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?</p>
