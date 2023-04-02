a511953ea60cbef7f1980d3caf1cd25ec998be66
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-02 14:08:41 -0600

    Update [id].js

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,16 +1,6 @@
-import Image from 'next/image';
+import { SingleEvents } from '@/src/components/events/SingleEvents';
 
-const Page = ({data}) => {
-  return (
-    <div>
-        <Image src={ data.image} width={900} height={500} alt={data.title} />
-        <h1>{data.title}</h1>
-        <p>{data.description}</p>
-        <input type="email" />
-        <button className="submit-id">submit</button>
-    </div>
-  )
-}
+const Page = ({data}) => <SingleEvents data={data} />
 
 export default Page;
 
