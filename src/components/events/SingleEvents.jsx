6503f22f70a2b0de892dba91f43d71f8f9c259c6
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-02 14:43:30 -0600

    api

--- src/components/events/SingleEvents.jsx
+++ src/components/events/SingleEvents.jsx
@@ -6,8 +6,11 @@ export const SingleEvents = ({data}) => {
         <Image src={ data.image} width={900} height={500} alt={data.title} />
         <h1>{data.title}</h1>
         <p>{data.description}</p>
-        <input type="email" />
-        <button className="submit-id">submit</button>
+        <div>
+          <label> Enroll for this event!</label>
+          <input type="email" />
+          <button className="submit-id">submit</button>
+        </div>
     </div>
   )
 }
