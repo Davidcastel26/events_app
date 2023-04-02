269757ab45441e72ae87fb09ae20145d339894eb
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-02 14:55:03 -0600

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -18,14 +18,18 @@ const Page = () => {
                     <h3>
                         Recusandae quia eos dolor vero
                     </h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?
-                </p>
-            </li>
-            <li>
-            <h3>At non eos laborum</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?</p>
-            </li>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae?
+                    </p>
+                </li>
+                <li>
+                    <h3>
+                        At non eos laborum
+                    </h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At non eos laborum praesentium molestias! Recusandae quia eos dolor vero non illo nobis. Reprehenderit delectus neque eum quos asperiores quibusdam vitae  
+                    </p>
+                </li>
             </ul>
         </div>
     )
